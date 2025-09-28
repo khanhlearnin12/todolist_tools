cd4c7b3aa0c887e4f40f21eec079990e14038eaa
khanhlearnin12 <hakhanh12.study@gmail.com>
2025-09-28 14:57:21 +0800

    command update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::{fs, io, path::Path};
 use std::io::Write;
-
+use chrono::{DateTime,Utc};
+//use chrono_tz::Asia::Taipei;
+use clearscreen;
 use serde::{Deserialize, Serialize};
 //libray to process the struct
 //derive ma hoa user duoi dang don gian nhat
 //tao struct de su ly chuooi nhap
-#[derive(Debug,Serialize,Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct ToDo {
+    date: DateTime<Utc>,
     text:String,
     done: bool,
 }
@@ -14,18 +17,22 @@ struct ToDo {
 
 impl ToDo{
     fn new(text: String) -> Self{
-        ToDo{text, done:false}
+        ToDo{
+            date:Utc::now(),
+            text,
+            done:false
+        }
     }
 }
 
 fn add_todo(todos: &mut Vec<ToDo>, new_text: String){
-    todos.push(ToDo::new(new_text.clone()));
+    todos.push(ToDo::new(new_text.clone())); 
     println!("add: {}",new_text);
 }
 
 fn remove_task(todos: &mut Vec<ToDo>, index: usize){
     if index > 0 && index <= todos.len(){
-        let remove = todos.remove(index - 1 );
+        let remove = todos.remove(index - 1 );  
         println!("Index {} have been remove",remove.text);
     } else {
         println!("Task not exsit");
@@ -89,7 +96,7 @@ fn main(){
     });
 
     loop{
-        println!("\n Commands: add, list, done, remove, edit, save, quit");
+        println!("\nCommands: add, list(ls), done, remove(rm), edit, save(sv), quit, clear");
         print!(">");
         io::stdout().flush().unwrap();
 
@@ -107,9 +114,13 @@ fn main(){
                     println!("Usage: add <task>");
                 }
             }
+            "ls" => {
+                list_todos(&todos);
+            }
             "list" => {
                 list_todos(&todos);
             }
+
             "done" => {
                 if part.len() > 1{
                     if let Ok(index) = part[1].parse::<usize>(){
@@ -121,6 +132,17 @@ fn main(){
                     println!("Usage: done <index>");
                 }
             }
+            "rm" => {
+                if part.len() > 1{ //
+                    if let Ok(index) = part[1].parse::<usize>(){
+                        remove_task(&mut todos, index);
+                    } else {
+                        println!("invalid index, use the number");
+                    } 
+                } else {
+                    println!("Usage: rm <index>");
+                }
+            }
             "remove" => {
                 if part.len() > 1{ //
                     if let Ok(index) = part[1].parse::<usize>(){
@@ -129,7 +151,7 @@ fn main(){
                         println!("invalid index, use the number");
                     } 
                 } else {
-                    println!("Usage: remove <index>");
+                    println!("Usage: rm <index>");
                 }
             }
             "edit" => {
@@ -150,6 +172,12 @@ fn main(){
                     println!("Usage: edit <index> <new task>");
                 }
             }
+            "sv" => {
+                match save_task(&mut todos, Path::new(todo_file_path)){
+                    Ok(_) => println!("To do list save successfully"),
+                    Err(e) => eprint!("Error saving todos:{}",e),
+                }
+            }
             "save" => {
                 match save_task(&mut todos, Path::new(todo_file_path)){
                     Ok(_) => println!("To do list save successfully"),
@@ -159,6 +187,10 @@ fn main(){
             "quit" => {
                 println!("Good bye!!");
                 break;
+            }
+            "clear" => {
+                clearscreen::clear().expect("Fail to clear screen");
+                println!("Screen cleared.");
             }
             _ => {
                 println!("Unknown Command!");
